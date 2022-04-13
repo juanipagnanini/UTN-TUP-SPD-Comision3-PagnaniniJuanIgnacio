e97e136638ddfbe24f6ddecd3728b4111755722e
Juan ignacio Pagnanini <102268738+juanipagnanini@users.noreply.github.com>
2022-04-13 20:16:18 -0300

    Add files via upload
    Clase 06-04-22 SPD

---
+++ 02-Clase-06-04-22/Act 1 Sist Proc de Datos.ino
@@ -0,0 +1,50 @@
+int LedRojo = 13;
+
+  void setup()
+{
+  pinMode(LedRojo, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LedRojo, HIGH);
+  delay(100);
+  digitalWrite(LedRojo, LOW);
+  delay(100);
+  digitalWrite(LedRojo, HIGH);
+  delay(200);
+  digitalWrite(LedRojo, LOW);
+  delay(200);
+  digitalWrite(LedRojo, HIGH);
+  delay(300);
+  digitalWrite(LedRojo, LOW);
+  delay(300);
+  digitalWrite(LedRojo, HIGH);
+  delay(400);
+  digitalWrite(LedRojo, LOW);
+  delay(400);
+  digitalWrite(LedRojo, HIGH);
+  delay(500);
+  digitalWrite(LedRojo, LOW);
+  delay(500);
+  digitalWrite(LedRojo, HIGH);
+  delay(600);
+  digitalWrite(LedRojo, LOW);
+  delay(600);
+  digitalWrite(LedRojo, HIGH);
+  delay(700);
+  digitalWrite(LedRojo, LOW);
+  delay(700);
+  digitalWrite(LedRojo, HIGH);
+  delay(800);
+  digitalWrite(LedRojo, LOW);
+  delay(800);
+  digitalWrite(LedRojo, HIGH);
+  delay(900);
+  digitalWrite(LedRojo, LOW);
+  delay(900);
+  digitalWrite(LedRojo, HIGH);
+  delay(1000);
+  digitalWrite(LedRojo, LOW);
+  delay(1000);
+}
